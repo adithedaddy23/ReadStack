f3efa2d8511bd36ae74eaaec6b8ad2dfa8c186de
adithedaddy23 <trivedi.adi09@gmail.com>
2025-07-14 12:28:03 +0530

    Added RetrofitInstance

--- app/src/main/java/com/example/readstack/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/readstack/api/RetrofitInstance.kt
@@ -1,4 +1,18 @@
 package com.example.readstack.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
+
+    private const val baseUrl ="https://openlibrary.org/"
+
+    private fun getInstance(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val bookApi: BookApi = getInstance().create(BookApi::class.java)
 }
