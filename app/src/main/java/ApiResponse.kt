5c9e4258177d2de2b83b1d8011fd34b1f2cdef23
adithedaddy23 <trivedi.adi09@gmail.com>
2025-07-14 12:04:43 +0530

    New Api Response

---
+++ app/src/main/java/ApiResponse.kt
@@ -0,0 +1,10 @@
+data class ApiResponse(
+    val docs: List<Doc>,
+    val documentation_url: String,
+    val numFound: Int,
+    val numFoundExact: Boolean,
+    val num_found: Int,
+    val offset: Any,
+    val q: String,
+    val start: Int
+)
---
+++ app/src/main/java/Doc.kt
@@ -0,0 +1,19 @@
+data class Doc(
+    val author_key: List<String>,
+    val author_name: List<String>,
+    val cover_edition_key: String,
+    val cover_i: Int,
+    val ebook_access: String,
+    val edition_count: Int,
+    val first_publish_year: Int,
+    val has_fulltext: Boolean,
+    val ia: List<String>,
+    val ia_collection_s: String,
+    val key: String,
+    val language: List<String>,
+    val lending_edition_s: String,
+    val lending_identifier_s: String,
+    val public_scan_b: Boolean,
+    val subtitle: String,
+    val title: String
+)
